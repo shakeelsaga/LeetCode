bcf5f4768e6f2ad9e6c4e150e124985ecfed16fe
shakeelsaga <shakeelmohammedofficial@gmail.com>
2025-01-27 21:58:55 +0530

    Added Q.151

---
+++ Easy/ReverseWordsInString.java
@@ -0,0 +1,22 @@
+package LeetCode.Easy;
+
+public class ReverseWordsInString {
+    public String reverseWords(String s) {
+        s = s.trim();
+        
+        // Split the string by spaces into an array of words
+        String[] words = s.split("\\s+");
+        
+        // Reverse the array of words
+        StringBuilder reversed = new StringBuilder();
+        
+        for (int i = words.length - 1; i >= 0; i--) {
+            reversed.append(words[i]);
+            if (i > 0) {
+                reversed.append(" ");
+            }
+        }
+        
+        return reversed.toString();
+    }
+}
