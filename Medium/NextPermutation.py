99577bfa7f64b5d170303692deda70a15a479c16
Mohammed Shakeel <shakeelmohammedofficial@gmail.com>
2025-12-06 12:08:41 +0530

    Added Q.31 solved in python

---
+++ Medium/NextPermutation.py
@@ -0,0 +1,30 @@
+class Solution:
+    def rev(self, arr, first, last):
+        while first < last:
+            arr[first], arr[last] = arr[last], arr[first]
+            first += 1
+            last -= 1
+
+    def nextPermutation(self, nums: List[int]) -> None:
+        index = -1
+        i = len(nums) - 1
+
+        while i > 0:
+            if nums[i] > nums[i - 1]:
+                index = i - 1
+                break
+            i -= 1
+
+        if index == -1:
+            self.rev(nums, 0, len(nums) - 1)
+            return
+        
+        i = len(nums) - 1
+        while i > index:
+            if nums[i] > nums[index]:
+                temp = nums[i]
+                nums[i] = nums[index]
+                nums[index] = temp
+                self.rev(nums, index + 1, len(nums) - 1)
+                return
+            i -= 1
